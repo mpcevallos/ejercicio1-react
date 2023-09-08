41a6fcc8f3e34252eed7312e6343c8882da4ccf3
mpcevallos <mpcevallos@gmail.com>
2023-09-08 12:28:53 -0500

    Corección de error en ruta de importación NavBar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import NavBar from './components/NavBar.jsx'
+import NavBar from './components/navBar.jsx'
 import SearchBar from './components/searchBar.jsx'
 import PostList from './components/postList.jsx'
 
