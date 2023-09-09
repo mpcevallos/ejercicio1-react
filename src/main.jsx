f491ca7fae872e7fc4497ae8a85b0b265b4197ce
mpcevallos <mpcevallos@gmail.com>
2023-09-09 03:36:52 -0500

    Se modifica el archivo main y se eliminar componentes para dejar solo el componente App

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import NavBar from './components/navBar.jsx'
-import SearchBar from './components/searchBar.jsx'
-import PostList from './components/postList.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <NavBar/>
-    <SearchBar/>
-    <PostList/>
   </React.StrictMode>
 )
