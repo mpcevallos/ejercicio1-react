a8d9440e95f98fbcfea3d0d2f53d754b352ebf39
mpcevallos <mpcevallos@gmail.com>
2023-09-09 03:35:52 -0500

    Se modifica el archivo App, importando los componentes NavBar, SearchBar y Pos tList

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import NavBar from "./components/navBar.jsx";
+import SearchBar from "./components/searchBar.jsx";
+import PostList from "./components/postList.jsx";
+
 function App() {
+  console.log("rendering App");
+
   return (
     <>
       <div className="container">
@@ -6,6 +12,9 @@ function App() {
         <h5>Descripción</h5>
       <h4>El objetivo del ejercicio es la construcción de una aplicación web React.js con diferentes components que tenga como resultado la siguiente interfaz de usuario (Mobile first):</h4>
       <br/>
+      <NavBar/>
+      <SearchBar/>
+      <PostList/>
       </div>
     </>
   )
